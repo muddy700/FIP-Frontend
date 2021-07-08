5f916dc78df3696f8c435557d4375da0a2a72625
Mohamed Mfaume <mohamedmfaume700@gmail.com>
2021-07-09 02:43:31 +0300

    Validated Questions Form

--- src/pages/admin/dashboardPage.js
+++ src/pages/admin/dashboardPage.js
@@ -20,6 +20,13 @@ export const DashboardPage = () => {
         isCorrect: ''
     }
 
+    const questionChoices = [
+        {id: 1, val: 'A'},
+        {id: 2, val: 'B'},
+        {id: 3, val: 'C'},
+        {id: 4, val: 'D'},
+    ]
+
     // const user = useSelector(selectUserData)
     const config = useSelector(apiConfigurations)
     // const currentDate = new Date().toLocaleDateString()
@@ -30,8 +37,10 @@ export const DashboardPage = () => {
     const [c2, setc2] = useState(initialChoice)
     const [c3, setc3] = useState(initialChoice)
     const [c4, setc4] = useState(initialChoice)
+    const [correctChoice, setcorrectChoice] = useState('')
     const [isSendingData, setIsSendingData] = useState(false)
     const [formError, setformError] = useState('')
+    const [successMessage, setsuccessMessage] = useState('')
 
       const fetchProfessions = async () => {
         try {
@@ -62,7 +71,7 @@ export const DashboardPage = () => {
             return false
         }
         else if (!c1.choice) {
-            setformError('Enter Correct Choice')
+            setformError('Enter First Choice')
             return false
         }
         else if (!c2.choice) {
@@ -77,10 +86,19 @@ export const DashboardPage = () => {
             setformError('Enter Fourth Choice')
             return false
         }
+        else if (!correctChoice) {
+            setformError('Select Correct Choice')
+            return false
+        }
         else {
             setformError('')
             return true
         }
+    }
+
+    const clearMessages = () => {
+        setformError('')
+        setsuccessMessage('')
     }
 
     const sendQuestion = async (e) => {
@@ -105,20 +123,24 @@ export const DashboardPage = () => {
 
     const sendMultipleChoices = async (qId) => {
 
-        const payloads = [
-            { ...c1, question: qId },
-            { ...c2, question: qId },
-            { ...c3, question: qId },
-            { ...c4, question: qId },]
+        const payloads =  [
+            { ...c1, question: qId, isCorrect: correctChoice === 'A' ? true : false },
+            { ...c2, question: qId, isCorrect: correctChoice === 'B' ? true : false  },
+            { ...c3, question: qId, isCorrect: correctChoice === 'C' ? true : false  },
+            { ...c4, question: qId, isCorrect: correctChoice === 'D' ? true : false  },
+        ]
+
         try {
             const responseArray = await addMultipleChoices(payloads, config)
-            console.log(responseArray)
+            console.log(responseArray.length)
             // setselectedProfession(null)
+            setsuccessMessage('Question Added Successsfull. ✔✔  👏 💪')
             setQuestionInfo(initialQuestion)
             setc1(initialChoice)
             setc2(initialChoice)
             setc3(initialChoice)
             setc4(initialChoice)
+            setcorrectChoice('')
             setIsSendingData(false)
         } catch (error) {
             console.log('sending Multiple Choices ', error.response.data )
@@ -140,7 +162,7 @@ export const DashboardPage = () => {
           <Form.Control as="select"
               size="md"
               value={selectedProfession}
-              onChange={e => { e.preventDefault(); setselectedProfession(e.target.value); setformError('') }}
+              onChange={e => { e.preventDefault(); setselectedProfession(e.target.value); clearMessages()}}
               name="profession">
               <option value={null}>---Select Profession---</option>
               {professions.map(skill => (
@@ -160,75 +182,102 @@ export const DashboardPage = () => {
                 name="post_description"
                 value={questionInfo.question_body}
                 aria-describedby="basic-addon2"
-                onChange={e => { e.preventDefault(); setQuestionInfo({ ...questionInfo, question_body: e.target.value }); setformError('') }}
+                onChange={e => { e.preventDefault(); setQuestionInfo({ ...questionInfo, question_body: e.target.value }); clearMessages() }}
             />
           </Form.Group>
       </Form.Row>
                     
         <Form.Row>
         <Form.Group as={Col} controlId="InternshipPostInput2">
-          <Form.Label>Correct Choice</Form.Label>
+          <Form.Label>First Choice (A)</Form.Label>
           <FormControl
-            placeholder="Enter Correct Choice"
+            placeholder="Enter First Choice"
             type="text"
             aria-label="Message Content"
             value={c1.choice}
             aria-describedby="basic-addon2"
-            onChange={e => { e.preventDefault(); setc1({ ...c1, choice: e.target.value, isCorrect: true}); setformError('') }}
+            onChange={e => { e.preventDefault(); setc1({ ...c1, choice: e.target.value}); clearMessages() }}
             />
           </Form.Group>
          {/* </Form.Row> */}
                     
         {/* <Form.Row> */}
         <Form.Group as={Col} controlId="InternshipPostInput2">
-          <Form.Label>Second Choice</Form.Label>
+          <Form.Label>Second Choice (B)</Form.Label>
           <FormControl
             placeholder="Enter Choice"
             type="text"
             aria-label="Message Content"
             value={c2.choice}
             aria-describedby="basic-addon2"
-            onChange={e => { e.preventDefault(); setc2({ ...c2, choice: e.target.value, isCorrect: false}) ; setformError('')}}
+            onChange={e => { e.preventDefault(); setc2({ ...c2, choice: e.target.value}) ; clearMessages()}}
             />
           </Form.Group>
          </Form.Row>
                     
         <Form.Row>
         <Form.Group as={Col} controlId="InternshipPostInput2">
-          <Form.Label>Third Choice</Form.Label>
+          <Form.Label>Third Choice (C)</Form.Label>
           <FormControl
             placeholder="Enter Choice"
             type="text"
             aria-label="Message Content"
             value={c3.choice}
             aria-describedby="basic-addon2"
-            onChange={e => { e.preventDefault(); setc3({ ...c3, choice: e.target.value, isCorrect: false}); setformError('') }}
+            onChange={e => { e.preventDefault(); setc3({ ...c3, choice: e.target.value}); clearMessages() }}
             />
           </Form.Group>
         {/* </Form.Row> */}
         {/* <Form.Row> */}
         <Form.Group as={Col} controlId="InternshipPostInput2">
-          <Form.Label>Fourth Choice</Form.Label>
+          <Form.Label>Fourth Choice (D)</Form.Label>
           <FormControl
             placeholder="Enter Choice"
             type="text"
             aria-label="Message Content"
             value={c4.choice}
             aria-describedby="basic-addon2"
-            onChange={e => { e.preventDefault(); setc4({ ...c4, choice: e.target.value, isCorrect: false}) ; setformError('')}}
+            onChange={e => { e.preventDefault(); setc4({ ...c4, choice: e.target.value}) ; clearMessages()}}
             />
           </Form.Group>
         </Form.Row>
     <Form.Row>
-    <Button
-        hidden={!formError}
-        variant='danger'
-        style={{width: '70%', marginRight: '5%'}}
-    >{formError}</Button>
-     <Button
-        type="submit"
-       >{isSendingData ? <Loader message='sending...!' /> : 'Send'} </Button>
-        </Form.Row>
+    <Form.Group as={Col} controlId="InternshipPostInput2">
+          <Form.Label>Correct Answer </Form.Label>
+            <ul>
+                {questionChoices.map((choice) => (
+                    <li key={choice.id} style={{ marginTop: '3px' }}>
+                        <Form.Check
+                            type="radio"
+                            id={choice.id}
+                            name="correctChoice"
+                            label={choice.val}
+                            value={choice.val}
+                            onChange={e => { e.preventDefault(); setcorrectChoice(e.target.value); clearMessages() }}
+                        />
+                    </li>))}
+            </ul> 
+          </Form.Group>
+    </Form.Row>
+    <Form.Row>
+            <Button 
+                type="submit"
+                hidden={formError || successMessage}
+                style={{marginBottom: '16px', width: '100%'}}
+            >{isSendingData ? <Loader message='sending...!' /> : 'Send'} </Button>
+    </Form.Row>
+    <Form.Row>
+        <Button
+            hidden={!formError}
+            variant='danger'
+            style={{width: '100%', marginBottom: '16px'}}
+        >{formError}</Button>
+        <Button
+            hidden={!successMessage}
+            variant='success'
+            style={{width: '100%'}}
+        >{successMessage}</Button>
+    </Form.Row>
         
                     </Form>
             </Card.Body>
